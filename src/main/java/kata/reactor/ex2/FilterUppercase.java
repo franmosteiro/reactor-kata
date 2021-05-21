94efa047c0155e75978e6d10ef786709cfb33a61
Fran Mosteiro <fran.mosteiro@holaluz.com>
2021-05-21 11:56:09 +0200

    ex2

--- src/main/java/kata/reactor/ex2/FilterUppercase.java
+++ src/main/java/kata/reactor/ex2/FilterUppercase.java
@@ -2,8 +2,10 @@ package kata.reactor.ex2;
 
 import reactor.core.publisher.Flux;
 
+import java.util.Locale;
+
 public class FilterUppercase {
     public static Flux<String> apply(Flux<String> data) {
-        return data;
+        return data.filter(s -> s.toUpperCase(Locale.ROOT).equals(s));
     }
 }
