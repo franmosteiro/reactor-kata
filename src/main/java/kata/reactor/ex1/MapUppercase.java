2fa402c1ad2be4a0ce4a691cf5a58498f2b6c771
Fran Mosteiro <fran.mosteiro@holaluz.com>
2021-05-21 11:56:00 +0200

    ex1

--- src/main/java/kata/reactor/ex1/MapUppercase.java
+++ src/main/java/kata/reactor/ex1/MapUppercase.java
@@ -2,8 +2,10 @@ package kata.reactor.ex1;
 
 import reactor.core.publisher.Flux;
 
+import java.util.Locale;
+
 public class MapUppercase {
     public static Flux<String> apply(Flux<String> data) {
-        return data;
+        return data.map(d -> d.toUpperCase(Locale.ROOT));
     }
 }
